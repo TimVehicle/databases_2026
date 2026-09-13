19b2a1e96584a1abf1237b6df68210087650435d
TimVehicle <shampyniksus@gmail.com>
2026-09-13 23:19:36 +0300

    Update hw1.sql

--- hw/hw1/hw1.sql
+++ hw/hw1/hw1.sql
@@ -39,7 +39,7 @@ CREATE TABLE Review (
     "id" SERIAL PRIMARY KEY, 
     "user_id" INTEGER NOT NULL REFERENCES "User"("id"), --  REFERENCES "User"(id): ссылка на таблицу "User" по полю id
     "course_id" INTEGER NOT NULL REFERENCES Course("id"), --  REFERENCES Course(id): ссылка на таблицу "Course" по полю id
-    "rating" INTEGER NOT NULL, 
+    "rating" INTEGER NOT NULL CHECK ("rating" BETWEEN 1 AND 5), -- CHECK ("rating" BETWEEN 1 AND 5): проверка, что рейтинг находится в диапазоне от 1 до 5
     "comment" TEXT, -- TEXT: строка произвольной длины для сообщения отзыва
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
 );
@@ -48,7 +48,20 @@ CREATE TABLE Payment (
     "id" SERIAL PRIMARY KEY, 
     "user_id" INTEGER NOT NULL REFERENCES "User"("id"), --  REFERENCES "User"(id): ссылка на таблицу "User" по полю id
     "course_id" INTEGER NOT NULL REFERENCES Course("id"), --  REFERENCES Course(id): ссылка на таблицу "Course" по полю id
-    "amount" DECIMAL(10,2) NOT NULL, -- DECIMAL(10,2): десятичное число, 10 знаков всего, 2 знака в дробной части, 
-    "platform_fee" DECIMAL(5,2) NOT NULL,  -- DECIMAL(5,2): десятичное число, 5 знаков всего, 2 знака в дробной части, например 10.00 - 10% от суммы платежа 
+    "amount" DECIMAL(10,2) NOT NULL CHECK ("amount" >= 0),
+    "platform_fee" DECIMAL(5,2) NOT NULL CHECK ("platform_fee" BETWEEN 0 AND 100),  -- DECIMAL(5,2): десятичное число, 5 знаков всего, 2 знака в дробной части, например 10.00 - 10% от суммы платежа 
     "paid_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
 );
+
+
+CREATE INDEX idx_course_teacher_id ON Course ("teacher_id");
+CREATE INDEX idx_lesson_course_id ON Lesson ("course_id");
+
+CREATE INDEX idx_enrollment_user_id ON Enrollment ("user_id");
+CREATE INDEX idx_enrollment_course_id ON Enrollment ("course_id");
+
+CREATE INDEX idx_review_user_id ON Review ("user_id");
+CREATE INDEX idx_review_course_id ON Review ("course_id");
+
+CREATE INDEX idx_payment_user_id ON Payment ("user_id");
+CREATE INDEX idx_payment_course_id ON Payment ("course_id");
